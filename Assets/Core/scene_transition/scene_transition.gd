f005e0678e7610f84a037b0108352d02f61f63e9
Giovanna Florin <80653026+GiFlorin@users.noreply.github.com>
2026-06-08 15:30:28 -0300

    phone mouse input bug fix

--- Assets/Core/scene_transition/scene_transition.gd
+++ Assets/Core/scene_transition/scene_transition.gd
@@ -1,4 +1,9 @@
 extends CanvasLayer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	$dissolve_rect.visible = false
+	$clouds.visible = false
 
 func change_scene(target: String, animation: String = 'dissolve') -> void:
 	match animation:
@@ -10,13 +15,17 @@ func change_scene(target: String, animation: String = 'dissolve') -> void:
 			get_tree().change_scene_to_file(target)
 
 func dissolve_transition(target: String):
-	$AnimationPlayer.play("dissolve")
-	await $AnimationPlayer.animation_finished
+	$dissolve_rect.visible = true
+	animation_player.play("dissolve")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file(target)
-	$AnimationPlayer.play_backwards("dissolve")
+	animation_player.play_backwards("dissolve")
+	$dissolve_rect.visible = false
 
 func clouds_transition(target: String):
-	$AnimationPlayer.play("clouds_in")
-	await $AnimationPlayer.animation_finished
+	$clouds.visible = true
+	animation_player.play("clouds_in")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file(target)
-	$AnimationPlayer.play("clouds_out")
+	animation_player.play("clouds_out")
+	$clouds.visible = false
