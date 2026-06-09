adc0b1761fb859cab8994afc8ed6638f189821f3
Giovanna Florin <80653026+GiFlorin@users.noreply.github.com>
2026-06-08 22:20:20 -0300

    better map animations and systems

--- Assets/Core/game_manager/game_manager.gd
+++ Assets/Core/game_manager/game_manager.gd
@@ -3,11 +3,17 @@ extends Node2D
 const destinations: Dictionary = {'destination 1':'res://Assets/Features/Destinations/Cities/city_1.tscn', 
 'destination 2': 'res://Assets/Features/Destinations/Cities/city_2.tscn', 
 'destination 3': 'res://Assets/Features/Destinations/Cities/city_3.tscn'}
-var money: int = 0
+var money: int = 10000
 var happiness: int = 0
-var cur_destination = ''
+var cur_destination = BEACH
 var previous_scene: String = ""
 var player_position: Vector2 = Vector2.ZERO
+
+enum {
+	BEACH,
+	LAKESIDE,
+	
+}
 
 # NPC relationship levels 0-3
 var test_npc_rel: int = 0
--- Assets/Core/scene_transition/scene_transition.gd
+++ Assets/Core/scene_transition/scene_transition.gd
@@ -1,10 +1,6 @@
 extends CanvasLayer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func _ready():
-	$dissolve_rect.visible = false
-	$clouds.visible = false
-
 func change_scene(target: String, animation: String = 'dissolve') -> void:
 	match animation:
 		'dissolve':
@@ -15,17 +11,13 @@ func change_scene(target: String, animation: String = 'dissolve') -> void:
 			get_tree().change_scene_to_file(target)
 
 func dissolve_transition(target: String):
-	$dissolve_rect.visible = true
 	animation_player.play("dissolve")
 	await animation_player.animation_finished
 	get_tree().change_scene_to_file(target)
 	animation_player.play_backwards("dissolve")
-	$dissolve_rect.visible = false
 
 func clouds_transition(target: String):
-	$clouds.visible = true
 	animation_player.play("clouds_in")
 	await animation_player.animation_finished
 	get_tree().change_scene_to_file(target)
 	animation_player.play("clouds_out")
-	$clouds.visible = false
