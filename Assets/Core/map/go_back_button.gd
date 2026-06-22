80b8c5608697f4ff21a1f9137f6226dd5c7f0c9c
Giovanna Florin <80653026+GiFlorin@users.noreply.github.com>
2026-06-22 14:39:47 -0300

    clean unused features

--- Assets/Core/map/go_back_button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-func _on_pressed() -> void:
-	close_map()
-
-func close_map():
-	if GameManager.previous_scene == "":
-		return  # safety check
-	SceneTransition.change_scene(GameManager.previous_scene)
--- Assets/Features/Destinations/Cities/city_lakeside.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _enter_tree() -> void:
-	GameManager.cur_destination = GameManager.LAKESIDE
